f0cc49e4fb94fdbe5584ee212020189e94f1a9a8
grant-16 <143277383+grant-16@users.noreply.github.com>
2024-03-18 16:32:40 -0400

    final equations added

--- ForceEquations.m
+++ ForceEquations.m
@@ -8,6 +8,7 @@ partCharge = 1;
 veloInit = 0;
 G = 9.81;
 EPSILON = 8.85418 * (10 ^ -12) 
+MAX_HEIGHT = 2
 
 % Constant
 gravity = (pi / 6.0) * partDensity * G * (diameter ^ 3);
@@ -15,6 +16,9 @@ buoyant = (pi / 6.0) * fluidDensity * G * (diameter ^ 3);
 plateField = (plateCharge * partCharge) / (2 * pi * EPSILON)
 
 % Time dependent
+veloApp = 1;
 reynoldsNum = (fluidDensity * abs(veloApp)) / viscosity;
 dragCoeff = 24.0 / reynoldsNum;
 drag = 0.5 * fluidDensity * dragCoeff * (pi / 4.0) * (diameter ^ 2) * (veloApp ^2);
+height = 1;
+otherField = (((partCharge ^ 2) * concentration) / 2 * EPSILON) * ((2 * height) - MAX_HEIGHT); 
