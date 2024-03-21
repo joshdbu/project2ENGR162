2962401d39d85d5254b4aadc5d8f74872eeb313d
grant-16 <143277383+grant-16@users.noreply.github.com>
2024-03-21 18:28:33 -0400

    color coding added

--- Python Code/Outflow Sim.py
+++ Python Code/Outflow Sim.py
@@ -8,24 +8,39 @@ allX = []
 allY = []
 
 magVel = 3
+windVel = -2
 filterHeight = 150
 filterSize = 100
 
-for height in range(filterHeight, filterHeight + filterSize + 1, 1):            
+for height in range(filterHeight, filterHeight + filterSize + 1, 100):            
     rand = random.randint(-5, 5)
     angle = - 10 + (round(height / 100.0, 1) - filterHeight / 100.0) * 20 + rand
     rad = (angle / 180) * m.pi
     xVel = magVel * m.cos(rad)
     yVel = magVel * m.sin(rad)
-    part = AirParticles(0, height / 100.0, xVel, yVel)
+    part = AirParticles(0, height / 100.0, xVel, yVel, windVel)
     t, x, y = part.euler()
     allTimes.append(t)
     allX.append(x)
     allY.append(y)
     
 for i in range(len(allTimes)):
+    """
+    deltaT = 15
+    colors = []
+    numTimes = len(allTimes[i])
+    b = 0
+    for j in range(numTimes):
+        if j < numTimes / 2:
+            r = round((j / (numTimes / 2)), 2)
+            g = 1
+        else:
+            r = 1
+            g = round(1 - ((j - (numTimes / 2)) / (numTimes / 2)), 2)
+        colors.append([r, g, b])
+    """
     plt.figure(1, dpi = 500)
-    plt.plot(allX[i], allY[i], linestyle = ':', color = 'r')
+    plt.scatter(allX[i], allY[i], c = 'r')
 
 plt.title('Y-position vs. X-position')
 plt.xlabel('X-position (cm)')
