5a9d651dc8607c5d8a5f9b25e4a9022e7f485e83
grant-16 <143277383+grant-16@users.noreply.github.com>
2024-03-21 16:35:17 -0400

    Fixed drag force calculations and added some randomness

--- Python Code/AirParticleClass.py
+++ Python Code/AirParticleClass.py
@@ -1,5 +1,6 @@
-from math import pi
-from math import sqrt
+import math as m
+from numpy import sign
+import random
 
 g = 9.81
 timeStep = 0.001 #input("Enter desired timestep in seconds: ")
@@ -12,10 +13,10 @@ class AirParticles:
         self.pfluid = 1.229
         self.mewFluid = 18.18 * 10 ** (-6)
         self.Cd = 0.47
-        self.pMass = (4/3) * pi * ((self.dpart / 2) ** 2) * self.ppart
+        self.pMass = (4/3) * m.pi * ((self.dpart / 2) ** 2) * self.ppart
         self.xPos, self.yPos = intX, intY
         self.xVel, self.yVel = intXVel, intYVel
-        self.vapt = sqrt(self.xVel ** 2 + self.yVel ** 2)
+        self.vapt = m.sqrt(self.xVel ** 2 + self.yVel ** 2)
         
     def euler(self):
         time = 0
@@ -23,18 +24,24 @@ class AirParticles:
         xList = [self.xPos]
         yList = [self.yPos]
         
+        angle = m.atan(self.yVel / self.xVel)
+        # print(angle)
+        randy = random.uniform(0.8, 1.2)
         n = 0
         while self.vapt > 0.5:
-            fdx = self.dragF(self.xVel)
-            fdy = self.dragF(self.yVel)
+            fdmag = self.dragF(self.vapt)
+            fdx = -1 * fdmag * m.cos(angle) 
+            fdy = -1 * fdmag * m.sin(angle) * randy
             self.updatePos(fdx, fdy)
             time = time + timeStep
             timeList.append(time)
             xList.append(self.xPos)
             yList.append(self.yPos)
+            #print(f"x: {self.xVel}   y: {self.yVel}   mag: {self.vapt}")
+            #print(self.vapt)
             n += 1
-        print(n * timeStep)
-        print(xList[-1])
+        #print(n)
+        
         return(timeList, xList, yList)
             
     def updatePos(self, fi, fk):
@@ -42,7 +49,8 @@ class AirParticles:
 
         self.xVel = ((fi * timeStep) / self.pMass) + oldXVel
         self.yVel = ((fk * timeStep) / self.pMass) + oldYVel
-        self.vapt = sqrt(self.xVel ** 2 + self.yVel ** 2)
+        #print(f"x: {self.xVel}   y: {self.yVel}")
+        self.vapt = m.sqrt(self.xVel ** 2 + self.yVel ** 2)
 
         avgXVel = (oldXVel + self.xVel) / 2
         avgYVel = (oldYVel + self.yVel) / 2
@@ -54,5 +62,5 @@ class AirParticles:
         self.yPos += deltaYPos
 
     def dragF(self, velo):
-        df = -1 * (1/2) * self.pfluid * self.Cd * (pi / 4) * self.dpart ** 2 * velo ** 2
+        df = (1/2) * self.pfluid * self.Cd * (m.pi / 4) * self.dpart ** 2 * velo ** 2
         return df
